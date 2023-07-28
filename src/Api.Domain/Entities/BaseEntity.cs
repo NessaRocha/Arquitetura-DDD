b09f0147273889d5ce158bf4f3e7f636129fabeb
Vanessa Rocha <87250768+NessaRocha@users.noreply.github.com>
2023-07-28 20:13:24 -0300

    .

--- src/Api.Domain/Entities/BaseEntity.cs
+++ src/Api.Domain/Entities/BaseEntity.cs
@@ -1,10 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Diagnostics;
-
-
+using System.ComponentModel.DataAnnotations;
 
 namespace Api.Domain.Entities
 {
