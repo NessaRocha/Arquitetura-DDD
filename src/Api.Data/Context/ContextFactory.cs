dd10c7b67c928ade1158dc772f9eab4f4e7d10c7
Vanessa Rocha <87250768+NessaRocha@users.noreply.github.com>
2023-07-28 21:55:45 -0300

    .

--- src/Api.Data/Context/ContextFactory.cs
+++ src/Api.Data/Context/ContextFactory.cs
@@ -3,13 +3,18 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Api.Data.Context
 {
+    public interface IContextFactory
+    {
+        MyContext CreateDbContext(string[] args);
+    }
+
     public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
     {
         public MyContext CreateDbContext(string[] args)
         {
             var connectionString = "Server=localhost;Port=3306;Database=dbAPI;Uid=root;Pwd=mudar@123";
             var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
-            optionsBuilder.UseMySql(connectionString);
+            object value = optionsBuilder.UseMySql(connectionString);
             return new MyContext(optionsBuilder.Options);
         }
     }
